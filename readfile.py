59592ede7df746aa8146d00db75b7d7bb42773d8
fedocx <fedocx2009@gmail.com>
2015-12-29 21:07:50 +0800

    change readfile.py

--- readfile.py
+++ readfile.py
@@ -3,7 +3,7 @@
 import ConfigParser
 import configure
 
-class readfile:
+class readfiles:
     def __init__():
         firstline=a
         lastline=a
@@ -33,7 +33,7 @@ class readfile:
         self.firstline=configure.getoption("FIRSTLINE",filename)
         self.lastline=configure.getoption("LASTLINE",filename)
         self.linenum=configure.getoption("LINENUM",filename)
-        
+
     def getvaluefromfile(filename)
         try:
             srcfile=open(filename,'r')
